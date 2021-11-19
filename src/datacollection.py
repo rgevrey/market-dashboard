5e66d7c69cf39889d2fd48c6f0c59d3c4d61b69a
rgevrey <romaingevrey@gmail.com>
2021-11-19 19:52:50 +0000

    Small function to save files to CSV added

--- src/datacollection.py
+++ src/datacollection.py
@@ -4,6 +4,7 @@
 import os
 import json
 import pandas as pd
+import csv
 
 def apidict(opmode, edit={}, debugverbose=False):
     """
@@ -63,25 +64,52 @@ def apidict(opmode, edit={}, debugverbose=False):
         raise ValueError("You want to edit but did not provide a dictionary.")
         return
                                 
-def dldocu(source=""):
+def dldocu(source="", save=False):
     """
     Downloads the available documentation and/or tickers to provide for the API
     calls needed by dldata where these are available.
-    - a
-    - b
-    - c
     Returns a file with the tickers or display the list of options
     Leave Source empty for list of available options
+    Save = "True" to save the 
     """
-    available = ["EUREX Futures"]
+    available = ["NASDAQ EUREX Futures"]
     
     if source == "":
         print("Documentation available on the following:")
         print(available)
         return
-    elif source == "EUREX Futures":
+    elif source == "NASDAQ EUREX Futures":
         docu = pd.read_csv("https://static.quandl.com/Ticker+CSV%27s/Futures/EUREX.csv",encoding='latin-1')
+        if save == True:
+            
     else:
         raise ValueError("The documentation requested is not valid, check what is available")
 
     return docu
+
+def savecsv(data, name, folder, mode="", debug=False):
+    """
+    Saves a dataframe to the requested folder
+    data: dataframe to be saved
+    name: name of the file to be created
+    mode: this is to decide what to do if the file already exists
+        overwrite: 
+        append: 
+        update:
+    location: folder where the file is to be created
+    debug: triggers verbose for troubleshooting
+    """
+    
+    # Builds path and file name
+    path = os.getcwd()
+    path = path.replace("\\src","\\" + folder + "\\" + name + ".csv")
+    path = path.replace("\\","\\\\")
+    if debug == True:
+        print("path:", path)
+        
+    # Saving to CSV!
+    data.to_csv(path)
+    
+    return
+    
+    
